2e7373deeede702edc936f0e1d638d376d7c44a3
Alex Felice <golfzander@comcast.net>
2020-11-01 09:36:28 -0600

    Deliverable 2 complete

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -27,3 +27,33 @@ INTO retiring_titles
 FROM emp_titles_retire
 GROUP BY title
 ORDER BY count DESC
+
+
+-- Deliverable #2: Mentorship Eligibility
+SELECT  DISTINCT ON (e.emp_no)
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibility
+FROM 	employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no
+
+
+
+
+
+
+
+
+
+
+
